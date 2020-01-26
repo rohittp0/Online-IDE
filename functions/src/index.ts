3def0d714b48f00f11b00b264635975755fd72d9
Rohit T P <tprohit9@gmail.com>
2020-01-26 15:31:04 +0530

    Sunday update

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,28 @@
 import * as functions from "firebase-functions";
+import { spawn } from "child_process";
+
+function runFile(code: string, response: functions.Response) {
+  const python = spawn("python3", ["-c", code]);
+  const responseJSON = { output: "", error: "", code: 0 };
+
+  python.stdin.end();
+  python.stdout.on("data", (data: string) => (responseJSON.output += data));
+  python.stderr.on(
+    "data",
+    (data: { toString: () => string }) =>
+      (responseJSON.error += data.toString())
+  );
+  python.on("close", (code: string) => {
+    responseJSON.code = Number(code);
+    response.json(responseJSON);
+  });
+}
 
 export const run = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
+  if (request.body.code) return runFile(request.body.code, response);
+  try {
+    if (JSON.parse(request.body).code)
+      return runFile(JSON.parse(request.body).code, response);
+  } catch (error) {}
+  return response.json({ error: "Please send a vaild post request." });
 });
