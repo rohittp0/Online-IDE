4d22efaacb2318797bb9e084ba36e2a08615a255
Rohit T P <tprohit9@gmail.com>
2020-01-27 19:11:55 +0530

    Restructured code

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,30 +1,32 @@
 import * as functions from "firebase-functions";
 import { spawn } from "child_process";
 
-function runFile(code: string, response: functions.Response) {
-  const python = spawn("python3", ["-c", code]);
+function runFile(code: string) {
   const responseJSON = { output: "", error: "", code: 0 };
 
-  python.stdin.end();
-  python.stdout.on("data", (data: string) => (responseJSON.output += data));
-  python.stderr.on(
-    "data",
-    (data: { toString: () => string }) =>
-      (responseJSON.error += data.toString())
-  );
-  python.on("close", (code: string) => {
-    responseJSON.code = Number(code);
-    response.json(responseJSON);
+  return new Promise((resolve, reject) => {
+    const python = spawn("python3", ["-c", code]);
+    python.stdin.end();
+    python.stdout.on("data", (data: string) => (responseJSON.output += data));
+    python.stderr.on(
+      "data",
+      (data: { toString: () => string }) =>
+        (responseJSON.error += data.toString())
+    );
+    python.on("close", (exitCode: string) => {
+      responseJSON.code = Number(exitCode);
+      resolve(responseJSON);
+    });
   });
 }
 
 export const run = functions.https.onRequest((request, response) => {
   response.set("Access-Control-Allow-Origin", "*");
   response.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  if (request.body.code) return runFile(request.body.code, response);
+  if (request.body.code) return runFile(request.body.code).then(response.json);
   try {
     if (JSON.parse(request.body).code)
-      return runFile(JSON.parse(request.body).code, response);
+      return runFile(JSON.parse(request.body).code).then(response.json);
   } catch (error) {}
   return response.json({ error: "Please send a vaild post request." });
 });
